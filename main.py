adcdf805d6ce0f41e8f8f7ad177ec5685b12f4f5
PiotrSlowikowski <peter.slowikowski@hotmail.com>
2023-01-21 12:41:43 +0100

    third commit

--- main.py
+++ main.py
@@ -1,3 +1,11 @@
-print('Hi')
-print("Hi guy")
-print("Another file")
+import os
+import time
+
+print(os.getcwd())
+os.chdir('E:\\Projekty\\python_wyklad\\exercise')
+print(os.getcwd())
+os.mkdir('new_folder')
+time.sleep(2)
+os.rename('new_folder', 'new_folder2')
+time.sleep(2)
+os.rmdir('new_folder2')
